cc8509b043e5245e150c68dcbe02393d380468c8
Mashrur Ahmed Yafi <yafi2475@gmail.com>
2018-04-13 19:11:32 +0600

    asd

--- app/src/main/java/com/practice/yafi/the200iqchallenge/Controller/GameActivity.kt
+++ app/src/main/java/com/practice/yafi/the200iqchallenge/Controller/GameActivity.kt
@@ -3,22 +3,29 @@ package com.practice.yafi.the200iqchallenge.Controller
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.Toast
+import com.practice.yafi.the200iqchallenge.Model.Question
 import com.practice.yafi.the200iqchallenge.R
-import com.practice.yafi.the200iqchallenge.Services.DataService
-
 import kotlinx.android.synthetic.main.activity_game.*
+import java.util.*
 
 class GameActivity : AppCompatActivity() {
     var currentQuestionNo = 0;
-    val questions = DataService.questions
+    private val questions = Vector<Question>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
-        init()
+
+        applicationContext.assets.open("QuestionFile.txt").reader().forEachLine {
+            println(it)
+            val strings = it.split(";")
+            questions.add(Question(strings[0], arrayOf(strings[1], strings[2], strings[3], strings[4]), strings[5].toInt()))
+        }
+        loadQuestion()
     }
 
-    fun init() {
+    private fun loadQuestion() {
         val currentQuestion =  questions[currentQuestionNo]
         questionText.text = currentQuestion.question
         optionButton1.text = currentQuestion.options[0]
@@ -29,30 +36,41 @@ class GameActivity : AppCompatActivity() {
 
     fun optionButton1Clicked(view: View) {
         if(questions[currentQuestionNo].answer == 1) {
-            currentQuestionNo++;
-            init();
+            currentQuestionNo++
+            loadQuestion()
+        } else {
+            wrongAnswer()
         }
     }
 
     fun optionButton2Clicked(view: View) {
         if(questions[currentQuestionNo].answer == 2) {
-            currentQuestionNo++;
-            init();
+            currentQuestionNo++
+            loadQuestion()
+        } else {
+            wrongAnswer()
         }
     }
 
     fun optionButton3Clicked(view: View) {
         if(questions[currentQuestionNo].answer == 3) {
-            currentQuestionNo++;
-            init();
+            currentQuestionNo++
+            loadQuestion()
+        } else {
+            wrongAnswer()
         }
     }
 
     fun optionButton4Clicked(view: View) {
         if(questions[currentQuestionNo].answer == 4) {
-            currentQuestionNo++;
-            init();
+            currentQuestionNo++
+            loadQuestion()
+        } else {
+            wrongAnswer()
         }
     }
 
+    fun wrongAnswer() {
+        Toast.makeText(this, "Wrong Answer", Toast.LENGTH_SHORT).show()
+    }
 }
